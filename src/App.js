0d479479990bcb03820f60cccbd766e5e00aa370
MicaelaInsfran02 <insfranmica00@gmail.com>
2026-04-24 18:05:09 -0300

    creacion de la funcion agregarCategoria, con sus acciones de editar y eliminar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
+//crear estado de categorias 
 function App() {
+  const [categorias, setCategorias] = useState([]);
+  const [nombreCategoria, setNombreCategoria] = useState("");
+  const [editandoId, setEditandoId] = useState(null); //editandoID guara que cat estoy editando
+  const [nuevoNombre, setNuevoNombre] = useState(""); //guarda el texto nuevo
+
+  //creamos la funcion para agregar categoria 
+  const agregarCategoria = () => {
+    if (nombreCategoria.trim() === "") return;
+
+      const nuevaCategoria = {
+        id: Date.now(), // ID automático
+        nombre: nombreCategoria
+      };
+    setCategorias([...categorias, nuevaCategoria]);
+    setNombreCategoria("");
+ };
+
+//creamos la funcion para eliminar categoria
+  const eliminarCategoria = (id) => {
+    const nuevasCategorias = categorias.filter(cat => cat.id !== id);
+    setCategorias(nuevasCategorias);
+  };
+
+//creamos la funcion para editar categoria, conservando el nombre actual en el input
+const guardarEdicion = (id) => {
+  const categoriasActualizadas = categorias.map(cat => {
+    if (cat.id === id) {
+      return { ...cat, nombre: nuevoNombre };
+    }
+    return cat;
+  });
+
+  setCategorias(categoriasActualizadas);
+  setEditandoId(null);
+  setNuevoNombre("");
+};
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Stock Manager</h1>
+      
+      <input
+        placeholder="Nombre de la categoría"
+        value={nombreCategoria}
+        onChange={(e) => setNombreCategoria(e.target.value)}
+       />
+      
+      <button onClick={agregarCategoria}>
+        Agregar Categoría
+      </button>
+      
+      <ul>
+     
+     </ul>
+            {categorias.map((cat) => (
+        <li key={cat.id}>
+
+          {editandoId === cat.id ? (
+            <>
+              <input
+                value={nuevoNombre}
+                onChange={(e) => setNuevoNombre(e.target.value)}
+              />
+              <button onClick={() => guardarEdicion(cat.id)}>
+                Guardar
+              </button>
+              <button onClick={() => setEditandoId(null)}>
+                Cancelar
+              </button>
+            </>
+          ) : (
+            <>
+              {cat.nombre}
+              <button onClick={() => {
+                setEditandoId(cat.id);
+                setNuevoNombre(cat.nombre);
+              }}>
+                Editar
+              </button>
+
+              <button onClick={() => eliminarCategoria(cat.id)}>
+                Eliminar
+              </button>
+            </>
+          )}
+
+        </li>
+      ))}
     </div>
   );
 }
